8234cb5fb7cd5106b92d43323b6227fdcf4005d8
Cem Koca <68961698+dcemkoca@users.noreply.github.com>
2025-04-25 00:11:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/triviaapp/ui/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.triviaapp.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.triviaapp.screens.QuestionsViewModel
+import com.example.triviaapp.screens.TriviaHome
+import com.example.triviaapp.ui.theme.MyappTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyappTheme {
+                TriviaHome()
+            }
+        }
+    }
+}
+
+
